95794432ada12c36561a9d7929792ae318b041e2
Katalkina-V-A <v.katalkina@mail.ru>
2016-05-06 03:08:51 +0300

    task20

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ validates :email, presence: true, uniqueness: {case_sensitive: false},
           format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 validates :role, presence: true, inclusion: {in: 0..ROLES.size}
 validates :sex, presence: true, inclusion: {in: 0..SEXES.size}
-validates :password, length: {minimum: 6, if: 'password.present?'}, presence: {on: :create}
+validates :password, presence: {on: :create}, format: {with: /\A(^(?=.*[a-z])(?=.*[0-9])).{6,}\z/}
 
 before_validation :set_default_role
 
